a7e0916064190b6d0ed4efc44bbe210a502e4170
Freekiehsoes <freekattema@live.nl>
2023-09-14 11:41:02 +0200

    changes

--- install.sh
+++ install.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
+
 GREEN='\033[0;32m'
 BOLD='\033[1m'
 RESET='\033[0m'
+CYAN='\033[0;36m'
+BLUE='\033[0;34m'
 
-function echon {
-  echo -e "$1"
-}
-
-echon "$BLUE$BOLD Installing the goto cli tool...$RESET"
-echon "$CYAN"
-echon "Creating ~/.tools directory if it does not exist..."
+echo -e "$BOLD$BLUE
+   ____           _           
+  / ___|   ___   | |_    ___  
+ | |  _   / _ \  | __|  / _ \ 
+ | |_| | | (_) | | |_  | (_) |
+  \____|  \___/   \__|  \___/ 
+$RESET$BLUE                              
+--------------------------------
+$RESET$CYAN$BOLD    Installing the goto cli $RESET$BLUE
+--------------------------------
+$RESET
+"
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p ~/.tools
-echon "$GREEN - Created $RESET$CYAN"
-echon "Creating ~/.tools/bin directory if it does not exist..."
 mkdir -p ~/.tools/bin
-echon "$GREEN - Created $RESET$CYAN"
-echon "Compiling goto..."
 go build $THISDIR 
-echon "$GREEN - Compiled $RESET$CYAN"
-echon "Moving goto to ~/.tools/bin"
 mv $THISDIR/goto ~/.tools/bin/
-echon "$GREEN - Moved $RESET$CYAN"
 
-echon "Adding function to zshrc,bashrc or bash_profile"
 FUNCFILE="$THISDIR/command.sh"
 function goFile {
   FILE=$1
@@ -47,11 +44,7 @@ goFile ~/.bashrc
 goFile ~/.zshrc
 goFile ~/.bash_profile
 
-echon "$GREEN - Added function to $RESET$CYAN"
-
-echon "\n\n\n"
-
-echon "$GREEN$BOLD Installation complete! $RESET"
-echon "$BLUE$BOLD Please restart your terminal to use the goto command $RESET"
-echon "$BLUE$BOLD You can also run the following command to refresh your terminal: $RESET"
-echon "$CYAN source ~/.zshrc (or .bashrc etc) $RESET"
+echo -e "$GREEN$BOLD Installation complete! $RESET"
+echo -e "$BLUE$BOLD Please restart your terminal to use the goto command $RESET"
+echo -e "$BLUE$BOLD You can also run the following command to refresh your terminal: $RESET"
+echo -e "$CYAN source ~/.zshrc (or .bashrc etc) $RESET"
